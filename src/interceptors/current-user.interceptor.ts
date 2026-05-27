686a93c5e2ab6dab52e4e1c42d6734d70dbebd23
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-05-27 12:18:57 +0300

    feat: add CurrentUser decorator and interceptor for retrieving the current user from session

---
+++ src/interceptors/current-user.interceptor.ts
@@ -0,0 +1,24 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { UsersService } from '../users/users.service';
+
+@Injectable()
+export class CurrentUserInterceptor implements NestInterceptor {
+  constructor(private usersService: UsersService) {}
+  async intercept(
+    context: ExecutionContext,
+    handler: CallHandler,
+  ): Promise<Observable<any>> {
+    const request = context.switchToHttp().getRequest();
+    const { userId } = request.session || {};
+    if (userId) {
+      request.currentUser = await this.usersService.findOne(userId);
+    }
+    return handler.handle();
+  }
+}
