a5595a9114df807b91e9c550e77bf523782914b0
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-05-27 11:49:50 +0300

    feat: integrate cookie-session middleware and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import cookieSession from 'cookie-session';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieSession({ keys: ['asdfasfd'] }));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
