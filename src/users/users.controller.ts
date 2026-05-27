e33296760611280fd04e5b0bb859e942d0c16270
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-05-27 10:28:27 +0300

    feat: implement custom serialization with Serialize interceptor and add UserDto for user response formatting

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,6 +14,8 @@ import {
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -24,7 +26,7 @@ export class UsersController {
     return this.usersService.create(body.email, body.password);
   }
 
-  @UseInterceptors(ClassSerializerInterceptor)
+  @Serialize(UserDto)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
