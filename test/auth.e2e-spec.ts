2f41e74b115c7af60a46e417580d0b271d9176c9
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-05-27 13:49:50 +0300

    test: add e2e test for auth signup flow and update environment-based configuration setup

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,31 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { App } from 'supertest/types';
+import { AppModule } from './../src/app.module';
+
+describe('Auth system (e2e)', () => {
+  let app: INestApplication<App>;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('handles a signup request', () => {
+    const email = 'asdf@asdf.com';
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'password' })
+      .expect(201)
+      .then((res) => {
+        const { id, email: resEmail } = res.body;
+        expect(id.toBeDefined());
+        expect(resEmail).toEqual(email);
+      });
+  });
+});
